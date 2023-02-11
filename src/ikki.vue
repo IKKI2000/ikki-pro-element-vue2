56dcd924bb140104f275f08d8b17b80821d721e3
IKKI2000 <ikki2002@qq.com>
2023-02-11 17:20:57 +0800

    【新增】配置全局 Loading 状态~

--- src/ikki.vue
+++ src/ikki.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="app">
+    <div id="app" v-loading.fullscreen.lock="$store.state.loading" :element-loading-text="$t('Common.Loading')">
         <nav>
             <router-link to="/">Home</router-link> |
             <router-link to="/about">About</router-link>
